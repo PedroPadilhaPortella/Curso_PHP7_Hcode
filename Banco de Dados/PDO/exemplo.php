bcddd03e7731834e84db5e678b50711353cf077c
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-17 19:23:13 -0300

    banco de dados

---
+++ Banco de Dados/PDO/exemplo.php
@@ -0,0 +1,15 @@
+<?php
+
+$connection = new PDO("mysql:dbname=hcode;host=localhost", "root", "");
+
+$statement = $connection->prepare("SELECT * FROM usuario ORDER BY id");
+
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($results as $row) {
+    foreach ($row as $key => $value) {
+       echo "<strong>$key</strong>: $value<br>";
+    }
+}
+
+echo json_encode($results);
