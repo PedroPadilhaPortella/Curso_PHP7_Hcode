5de768462b7617b738192ddae2b99ebf764ffe0a
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-07-25 17:27:51 -0300

    select e update

--- Banco de Dados/Data Acess Object/Usuario.php
+++ Banco de Dados/Data Acess Object/Usuario.php
@@ -1,7 +1,158 @@
-<?php
+<?php 
 
 class Usuario {
-    
-}
 
-?>
+	private $id;
+	private $nome;
+	private $senha;
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+
+	public function getSenha(){
+		return $this->senha;
+    }
+
+	public function setSenha($senha){
+		$this->senha = $senha;
+    }
+
+	
+	public function loadById($id){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM usuario WHERE id = :ID", array(
+			":ID"=>$id
+		));
+
+		if (count($results) > 0) {
+
+			$this->setData($results[0]);
+
+		}
+
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM usuario ORDER BY nome;");
+
+	}
+
+	public static function search($nome){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM usuario WHERE nome LIKE :SEARCH ORDER BY nome", array(
+			':SEARCH'=>"%".$nome."%"
+		));
+
+	}
+
+	public function login($nome, $senha){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM usuario WHERE nome = :LOGIN AND senha = :PASSWORD", array(
+			":LOGIN"=>$nome,
+			":PASSWORD"=>$senha
+		));
+
+		if (count($results) > 0) {
+
+			$this->setData($results[0]);
+
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos.");
+
+		}
+
+	}
+
+	public function setData($data){
+
+		$this->setId($data['id']);
+		$this->setNome($data['nome']);
+		$this->setSenha($data['senha']);
+	}
+
+	public function insert(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getNome(),
+			':PASSWORD'=>$this->getSenha()
+		));
+
+		if (count($results) > 0) {
+			$this->setData($results[0]);
+		}
+
+	}
+
+	public function update($login, $password){
+
+		$this->setNome($login);
+		$this->setSenha($password);
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE usuario SET nome = :LOGIN, senha = :PASSWORD WHERE id = :ID", array(
+			':LOGIN'=>$this->getNome(),
+			':PASSWORD'=>$this->getSenha(),
+			':ID'=>$this->getId()
+		));
+
+	}
+
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM usuario WHERE id = :ID", array(
+			':ID'=>$this->getId()
+		));
+
+		$this->setId(0);
+		$this->setNome("");
+		$this->setSenha("");
+
+	}
+
+	public function __construct($login = "", $password = ""){
+
+		$this->setNome($login);
+		$this->setSenha($password);
+
+	}
+
+	public function __toString(){
+
+		return json_encode(array(
+			"id"=>$this->getId(),
+			"nome"=>$this->getNome(),
+			"senha"=>$this->getSenha(),
+		));
+
+	}
+
+}
+ ?>
--- Banco de Dados/Data Acess Object/index.php
+++ Banco de Dados/Data Acess Object/index.php
@@ -1,8 +1,48 @@
-<?php
+<?php 
 
-require_once "config.php";
+require_once("config.php");
 
-$sql = new Conexao();
-$usuarios = $sql->select("SELECT * FROM gafanhotos");
-echo json_encode($usuarios);
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("pedro");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("pedro", "phpp.2001");
+//echo $usuario;
+
+
+//Criando um novo usuário
+//$aluno = new Usuario("marcos", "@lun0");
+//$aluno->insert();
+//echo $aluno;
+
+
+//Alterar um usuário
+$usuario = new Usuario();
+$usuario->loadById(1);
+$usuario->update("jose", "!@#$%¨&*");
+echo $usuario;
+
+
+/*
+//deleta um usuario
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
+*/
+
 ?>
